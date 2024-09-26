90fe29ca1174887559fdac13a93f8a57479afe76
exam <exam@Aptechnk1.edu>
2024-09-26 17:46:22 +0500

    Laiba

--- project/lib/welcomeScreen.dart
+++ project/lib/welcomeScreen.dart
@@ -7,12 +7,23 @@ class Welcomescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.black,
         height: 400,
         width: 350,
-        child: const Text(
-          "Laiba",
-          style: TextStyle(color: Colors.white),
+        decoration: const BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+        ),
+        child: const Center(
+          child: Text(
+            "Laiba", //  /n for line break in text
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 40,
+              fontWeight: FontWeight.w700,
+              letterSpacing: 3,
+            ),
+          ),
         ),
       ),
     );
